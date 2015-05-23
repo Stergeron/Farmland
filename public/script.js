a460e634baebf6f1ea94b4300572647c3630c6a4
a9 <themaxaxis@gmail.com>
2015-05-23 18:48:16 -0400

    frontend draft

---
+++ public/script.js
@@ -0,0 +1,32 @@
+var socket = io();
+
+var game = new Vue({
+  el: "#ui",
+  data: {
+    farm: [
+
+    ],
+    inventory: [
+      {name: "seed"},{name: "poop"}
+    ],
+    money: 100,
+    spectating: false
+  },
+  methods: {
+    populateFarm: function() { //TEST UNTIL SERVER WERKS
+      for (var y = 0; y < 10; y++) {
+        this.farm.push([]);
+        for (var x = 0; x < 10; x++) {
+          this.farm[y].push([]);
+          this.farm[y][x] = {};
+        }
+      }
+    }
+  }
+});
+
+socket.emit("joinGame", "bob", function(farm){
+  if(farm){
+    game.farm = farm;
+  }
+});
