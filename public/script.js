b5944d8dc937a7075ab8742082c0742d434f9b4c
William Tan <williamjefftan@gmail.com>
2015-05-23 23:02:11 -0400

    changed to indentations

--- public/script.js
+++ public/script.js
@@ -1,46 +1,46 @@
 var socket = io();
 
 var game = new Vue({
-  el: "#ui",
-  data: {
-    player: {},
-    planting: "",
-    name: "bob",
-    pw: "blop"
-  },
-  methods: {
-    plantingMode: function(seed) {
-      this.planting = seed;
-    },
-    plant: function(tile) {
-      if (this.planting !== "") {
-        var _this = this;
-        socket.emit("plantFood", this.name, this.planting.plant, tile.row, tile.col, function(cb) {
-          if (cb) {
-            _this.player.farm[tile.row][tile.col] = _this.planting.plant;
-            _this.player.inventory[_this.player.inventory.indexOf(_this.planting)].quantity--;
-            if(_this.planting.quantity < 1) _this.planting = "";
-          }
-        });
-      }
-    },
-    fillFarm: function(farm) {
-      this.player = farm;
-    }
-  },
-  filters: {
-    debug: function(val){
-      console.log(val.name);
-      return true;
-    }
-  }
+	el: "#ui",
+	data: {
+		player: {},
+		planting: "",
+		name: "bob",
+		pw: "blop"
+	},
+	methods: {
+		plantingMode: function(seed) {
+			this.planting = seed;
+		},
+		plant: function(tile) {
+			if (this.planting !== "") {
+				var _this = this;
+				socket.emit("plantFood", this.name, this.planting.plant, tile.row, tile.col, function(cb) {
+					if (cb) {
+						_this.player.farm[tile.row][tile.col] = _this.planting.plant;
+						_this.player.inventory[_this.player.inventory.indexOf(_this.planting)].quantity;
+						if(_this.planting.quantity < 1) _this.planting = "";
+					}
+				});
+			}
+		},
+		fillFarm: function(farm) {
+			this.player = farm;
+		}
+	},
+	filters: {
+		debug: function(val){
+			console.log(val.name);
+			return true;
+		}
+	}
 });
 
 socket.emit("createFarm", game.name, game.pw, function(farm) {
-  if (farm) {
-    game.fillFarm(farm);
-  } else {
-    console.error("SOMEONE ELSE LOGGED IN DINGUS");
-  }
+	if (farm) {
+		game.fillFarm(farm);
+	} else {
+		console.error("SOMEONE ELSE LOGGED IN DINGUS");
+	}
 });
 game.$watch('player.farm', function(){console.log("updated")}, true);
