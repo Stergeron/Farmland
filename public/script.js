6e5083bf587dd6005a366dd5cd4bf51f977845a2
a9 <themaxaxis@gmail.com>
2015-05-24 03:01:40 -0400

    Revert cf92f04..4e87ce2
    This rolls back to commit cf92f047859a2145f74b7e7fda51313778948ee3.

--- public/script.js
+++ public/script.js
@@ -13,7 +13,6 @@ var game = new Vue({
       this.planting = seed;
     },
     touch: function(tile) {
-      console.log(tile);
       if (this.planting === "") {
         this.pick(tile);
       } else {
